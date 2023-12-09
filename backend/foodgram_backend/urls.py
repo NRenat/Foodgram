3adbc314fe76574afc63e0e73e4f1e98c4eeb8d3
NRenat <nrenat1606@yandex.ru>
2023-12-09 22:51:41 +0300

    flake8

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -1,8 +1,5 @@
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-
-from foodgram_backend import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
