a6c99da5c8ee832544dcc760ee544bca46a7445c
NRenat <nrenat1606@yandex.ru>
2023-12-10 21:40:24 +0300

    quick fix

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -75,7 +75,7 @@ DATABASES = {
     }
 }
 
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(', ')
 
 AUTH_PASSWORD_VALIDATORS = [
     {
